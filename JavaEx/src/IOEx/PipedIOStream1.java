1da56fabf030d712330e31078c6f7f5891aa1d4b
D-DO <osmonkulov@mail.ru>
2018-03-27 15:42:42 +0600

    String

---
+++ JavaEx/src/IOEx/PipedIOStream1.java
@@ -0,0 +1,25 @@
+package IOEx;
+
+public class PipedIOStream1 {
+    public static void main(String[] args) {
+//        // inStream - объект класса PipedInputStream
+//        try {
+//            while(true) {
+//                byte[] readedBytes = null;
+//                synchronized(inStream) {
+//                    int bytesAvailable = inStream.available();
+//                    readedBytes = new byte[bytesAvailable];
+//                    inStream.read(readedBytes);
+//                }
+//                // обработка полученных данных из readedBytes
+//                // …
+//            } catch(IOException e) {
+//
+///* IOException будет брошено, когда поток inStream, либо
+//связанный с ним PipedOutputStream, уже закрыт, и при этом
+//производится попытка считывания из inStream */
+//
+//                System.out.println("работа с потоком inStream завершена");
+//            }
+  }
+}
